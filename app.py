98cbf81df489af90122731aa595aeeb44deee6a7
herculesc <herculesc635@gmail.com>
2022-07-20 19:10:52 -0300

    Levantamento de requisito para o programa

---
+++ app.py
@@ -0,0 +1,19 @@
+"""
+Controle de horas, um ponto eletrônico que calcula
+a quantidade de horas trabalhadas.
+
+É necessario armazenar a data -> calcular a
+
+hora de entrada - hora de saida do trabalho
+
+"""
+from datetime import date, time
+hoje = date.today()
+print(hoje)
+marcar_data = date.today()
+marcar_tempo = time()
+
+
+
+
+
